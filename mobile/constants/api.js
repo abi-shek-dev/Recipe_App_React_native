fc10fd7c1e239d95c22d155e10fd55867343669d
Abishek M ( Xe54z ) <abishekinterspace@gmail.com>
2026-03-25 22:23:07 +0530

    api

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,5 +1,1 @@
-// if you're using your physical phone, change this to the deployed url
-// we have explained this in the course :-)
-// Use 10.0.2.2 for Android Emulator
-//qv: api.js
 export const API_URL = "https://recipe-app-react-native.vercel.app/api";
